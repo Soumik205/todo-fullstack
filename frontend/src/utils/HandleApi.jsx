d855e8fbf531536ae42470fb5f1944587d37f14e
Soumik205 <soumikghosh205@gmail.com>
2023-11-27 00:23:11 +0600

    read baseurl from env

--- frontend/src/utils/HandleApi.jsx
+++ frontend/src/utils/HandleApi.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:5000";
+const baseUrl = import.meta.env.BASE_URL;
 
 const getAllToDo = (setToDo) => {
   axios.get(baseUrl).then(({ data }) => {
